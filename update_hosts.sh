e020a81e7ef06baf554bfb6a8443c6026f9b7b76
dinghao <18001326539@163.com>
2017-02-21 11:57:10 +0800

    fix logic

--- update_hosts.sh
+++ update_hosts.sh
@@ -3,4 +3,5 @@ update_hosts="wget -c -O /etc/hosts-pc https://raw.githubusercontent.com/tonyden
 cat  /etc/hosts-pc > /etc/hosts-pc_bak
 echo '' > /etc/hosts-pc
 $update_hosts
-{ [ $? -eq 0 ] || sleep 1 && $update_hosts ; } && { sleep 1 && /bin/systemctl restart dnsmasq ;} || cat /etc/hosts-pc_bak > /etc/hosts-pc
+{ [ $? -eq 0 ] || { sleep 1 && $update_hosts ;} ; } && { sleep 1 && /bin/systemctl restart dnsmasq ;} || cat /etc/hosts-pc_bak > /etc/hosts-pc
+
