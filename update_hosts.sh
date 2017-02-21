ac92994d8697f2f5b6fd9064ff1006b99920bc84
dinghao <18001326539@163.com>
2017-02-21 11:53:30 +0800

    update update-files,add a try

--- update_hosts.sh
+++ update_hosts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
+update_hosts="wget -c -O /etc/hosts-pc https://raw.githubusercontent.com/tonydeng/2017hosts/master/hosts-pc"
 cat  /etc/hosts-pc > /etc/hosts-pc_bak
 echo '' > /etc/hosts-pc
-wget   -c -O /etc/hosts-pc https://raw.githubusercontent.com/tonydeng/2017hosts/master/hosts-pc
-[ $? -eq 0 ] && { sleep 1 && /bin/systemctl restart dnsmasq ;} || cat /etc/hosts-pc_bak > /etc/hosts-pc
+$update_hosts
+{ [ $? -eq 0 ] || sleep 1 && $update_hosts ; } && { sleep 1 && /bin/systemctl restart dnsmasq ;} || cat /etc/hosts-pc_bak > /etc/hosts-pc
