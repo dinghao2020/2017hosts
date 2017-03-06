c3b359e1debfed4247e183105eebc4ad82c6fdf5
dinghao2020 <18001326539@163.com>
2017-03-06 14:37:33 +0800

    Delete update_hosts.sh

--- update_hosts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-update_hosts="wget -c -O /etc/hosts-pc https://raw.githubusercontent.com/tonydeng/2017hosts/master/hosts-pc"
-cat  /etc/hosts-pc > /etc/hosts-pc_bak
-echo '' > /etc/hosts-pc
-$update_hosts
-{ [ $? -eq 0 ] || { sleep 1 && $update_hosts ;} ; } && { sleep 1 && /bin/systemctl restart dnsmasq ;} || cat /etc/hosts-pc_bak > /etc/hosts-pc
-
